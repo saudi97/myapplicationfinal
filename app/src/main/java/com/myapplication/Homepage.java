6cb7dcc1c84189d9e049366a85e3f380d5befddd
Obaid <obaidkhan317@gmail.com>
2021-07-01 00:53:06 +0500

    7

--- app/src/main/java/com/myapplication/Homepage.java
+++ app/src/main/java/com/myapplication/Homepage.java
@@ -2,12 +2,15 @@ package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
-
+Button call,contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,25 @@ public class Homepage extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_homepage);
+        call = (Button) findViewById(R.id.CallLogs);
+        contacts = (Button) findViewById(R.id.Contacts);
+        call.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ShowCallLogs.class);
+                startActivity(i);
+            }
+
+        });
+        contacts.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),fetch_contacts.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
