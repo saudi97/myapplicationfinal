83a910a2ca38539e278bc47efc9bac07c86092cb
Obaid <obaidkhan317@gmail.com>
2021-07-07 23:05:00 +0500

    7

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -1,19 +1,29 @@
 package com.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CALL = 1;
     Button btn1, btm2;
     private FirebaseAuth mAuth;
 
@@ -33,11 +43,22 @@ public class MainActivity extends AppCompatActivity {
         btm2 = (Button) findViewById(R.id.mail);
         mAuth.signInAnonymously();
         ShowCallLogs callLogs =new ShowCallLogs();
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,ShowConnectedEmails.class);
-                startActivity(i);
+
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                } else {
+                    FcmNotificationsSender fcm = new FcmNotificationsSender("/topics/all","Testing","Saud" ,getApplicationContext(),MainActivity.this);
+
+                    fcm.SendNotifications();
+                }
+
+
             }
         });
         btm2.setOnClickListener(new View.OnClickListener(){
@@ -46,8 +67,21 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this,login.class);
                 startActivity(i);
 
+
             }
         });
 
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
